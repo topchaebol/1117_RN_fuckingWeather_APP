c0c2b00371ce8825f21a386b18e1edd341d648ae
HA HEEMOK <28106168+topchaebol@users.noreply.github.com>
2019-11-17 14:06:03 +0900

    init

---
+++ Weather.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import {Ionicons} from '@expo/vector-icons';
+import PropTypes from 'prop-types';
+
+const WeatherCases = {
+    Rain : {
+        colors : ["#00C6FB","#005BEA"],
+        title : "Raining like a MF",
+        subtitle : "For more info look outside",
+        icon : "ios-rainy"
+    },
+    Clear : {
+        colors : ["#FEF253","#FF7300"],
+        title : "Sunny as a Thun",
+        subtitle : "Get your ass burnt",
+        icon : "ios-sunny"
+    }, 
+    ThunderStorm : {
+        colors : ["#00ECBC","#007ADF"],
+        title : "ThunderStorm in the house",
+        subtitle : "Actually outside of the house",
+        icon : "ios-thunderstorm"
+    },
+    Clouds : {
+        colors : ["#D7D2CC","#304352"],
+        title : "Clouds",
+        subtitle : "I know fucking boring",
+        icon : "ios-cloudy"
+    },
+    Snow : {
+        colors : ["#7DE2FC","#B9B6E5"],
+        title : "Cold as balls",
+        subtitle : "Do you want to build a strong man? Fuck yes",
+        icon : "ios-snow"
+    },
+    Drizzle : {
+        colors : ["#89F7FE","#66A6FF"],
+        title : "Drizzle",
+        subtitle : "Its like a rain, but g",
+        icon : "ios-umbrella"
+    },
+}
+
+function Weather({weatherName, temp}){
+    console.log(weatherName);
+    return(
+        <LinearGradient 
+            colors = {WeatherCases[weatherName].colors} 
+            style={styles.container}>
+            <View style = {styles.upper}>
+                <Ionicons color="white" size={145} name={WeatherCases[weatherName].icon}/>
+                <Text style={styles.temp}>{temp}℃</Text>
+            </View>
+            <View style = {styles.lower}>
+                <Text style={styles.title}>{WeatherCases[weatherName].title}</Text>
+                <Text style={styles.subtitle}>{WeatherCases[weatherName].subtitle}</Text>
+            </View>
+        </LinearGradient>
+    );
+}
+Weather.propTypes = {
+    temp :PropTypes.number.isRequired,
+    weatherName : PropTypes.string.isRequired
+}
+export default Weather;
+const styles = StyleSheet.create({
+    container : {
+        flex : 1
+    },
+    upper : {
+        flex : 1,
+        alignItems : 'center',
+        justifyContent : 'center',
+        backgroundColor : 'transparent'
+    },
+    temp : {
+        fontSize : 48,
+        backgroundColor : "transparent",
+        color : 'white',
+        marginBottom : 24,
+        marginTop : 10
+    },
+    lower : {
+        flex : 1,
+        alignItems : 'flex-start',
+        justifyContent : 'flex-end',
+        paddingLeft : 25
+    },
+    title : {
+        fontSize : 38,
+        backgroundColor : "transparent",
+        color : 'white',
+        marginBottom : 10,
+        fontWeight : "300"
+    },
+    subtitle : {
+        fontSize : 24,
+        backgroundColor : "transparent",
+        color : 'white',
+        marginBottom : 24
+    }
+});
